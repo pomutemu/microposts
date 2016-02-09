1da18349ba1b60ceb4e4aebe418512d41163fc8f
pomutemu <pomutemu@users.noreply.github.com>
2016-02-09 16:56:10 +0900

    Add location and profile to User

---
+++ db/migrate/20160209075058_add_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :location, :string
+    add_column :users, :profile, :string
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
     email "email@example.com"
     password "password"
     password_confirmation "password"
+    location "location"
+    profile "profile"
 
     factory :user_alice do
       name "alice"
