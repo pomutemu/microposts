29a18505d28cc7884cb1cc5687d68e44f4caba27
pomutemu <pomutemu@users.noreply.github.com>
2016-02-20 19:34:18 +0900

    Show timeline to users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -9,6 +9,8 @@ class MicropostsController < ApplicationController
 
       redirect_to root_url
     else
+      @feed_items = (current_user.feed_items.includes :user).order created_at: :desc
+
       render "static_pages/home"
     end
   end
